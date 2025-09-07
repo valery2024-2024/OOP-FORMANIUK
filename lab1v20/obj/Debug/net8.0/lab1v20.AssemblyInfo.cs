41544ca2508d6f5050d39687294e1b963d11e5cd
valera2025 <sof20b13@gmail.com>
2025-09-07 23:37:45 +0300

    Оновлення проєкту

--- lab1v20/obj/Debug/net8.0/lab1v20.AssemblyInfo.cs
+++ lab1v20/obj/Debug/net8.0/lab1v20.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1v20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12345ec320137b7d955de87f7c1b2b4d6032d59e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c677d5758af6963476ce8432a9645f75c31c75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1v20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1v20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
