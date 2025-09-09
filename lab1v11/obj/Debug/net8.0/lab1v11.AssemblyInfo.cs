51d8564fb6f8c49de65b681b67dd90d5f1951f43
valera2025 <sof20b13@gmail.com>
2025-09-10 01:27:40 +0300

    Оновлення

--- lab1v11/obj/Debug/net8.0/lab1v11.AssemblyInfo.cs
+++ lab1v11/obj/Debug/net8.0/lab1v11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1v11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a935571f73f14e1a0690c43612a14bc59ef967a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad36d450657c2a219ac6eeb9d565b3788590866")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1v11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1v11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
